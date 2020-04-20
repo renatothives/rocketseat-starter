180a3f25753ef48ecc0d192daedb83ce0e8abccb
Renato Thives <renato.ti@outlook.com.br>
2020-04-20 20:59:37 -0300

    Recuperando elementos do TO-DO

--- todo/todos.js
+++ todo/todos.js
@@ -1,1 +1,9 @@
-alert("Funcionou");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = [
+    "Fazer café",
+    "Estudar javascript",
+    "Acessar comunidade"
+];
