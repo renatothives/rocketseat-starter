53d7cdfd380905d791a62d1499c99106964d6607
Renato Thives <renato.ti@outlook.com.br>
2020-04-20 21:44:01 -0300

    Excluindo TO-DO

--- todo/todos.js
+++ todo/todos.js
@@ -14,8 +14,18 @@ function renderTodos(){
   for (todo of todos){
     var todoElement = document.createElement("li");
     var todoText= document.createTextNode(todo);
+
+    var linkElement = document.createElement("a");
+    var linkText = document.createTextNode("-Excluir");
     
+    var pos = todos.indexOf(todo);
+
+    linkElement.setAttribute("href","#");
+    linkElement.setAttribute("onclick","deleteTodo(" + pos + ")");
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
     listElement.appendChild(todoElement);
   }
 }
@@ -26,6 +36,13 @@ function addTodo(){
   // Adicionar um valor no final do array
   todos.push(todoText);
   inputElement.value = "";
+  renderTodos();
+}
+
+function deleteTodo(pos){
+  // método que remove uma determinada quantida de itens do array (segundo parâmetro), iniciando com base no primeiro parâmetro.
+  todos.splice(pos,1);
+
   renderTodos();
 }
 
