3bcba41d8ae243f00a927748b589930c440ac5ef
Renato Thives <renato.ti@outlook.com.br>
2020-04-21 11:56:19 -0300

    Requisições AJAX

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,11 @@
 var xhr = new XMLHttpRequest(); // classe que permite utilizar o AJAX
 
+//AJAX é uma requisição assíncrona
 xhr.open("GET","https://api.github.com/users/renatothives");
 xhr.send(null);
+
+xhr.onreadystatechange = function(){
+  if (xhr.readyState === 4) {
+    console.log(JSON.parse(xhr.responseText));
+  }
+}
