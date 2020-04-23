4638cd6aee5e96269ae8e44ad7973cd0e20a0dda
Renato Thives <renato.ti@outlook.com.br>
2020-04-23 14:40:41 -0300

    Desafio Módulo 04 - Exercício 01 - Promise.

--- javascript/desafio/modulo04Exercicio01.js
+++ javascript/desafio/modulo04Exercicio01.js
@@ -1,33 +1,9 @@
 /*  Crie uma função que recebe a idade de um usuário e retorna uma Promise que depois de 2
     segundos retornará se usuário é maior ou não que 18 anos. Se o usuário ter mais que 18 anos de
     idade o resultado deve cair no .then, caso contrário, no .catch  --*/
-
-
-var minhaPromise = function(){
-  return new Promise(function(resolve, reject){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET","https://api.github.com/users/renatothives");
-    xhr.send(null);
-
-
-//AJAX é uma requisição assíncrona
-    xhr.onreadystatechange = function(){
-xhr.open("GET","https://api.github.com/users/renatothives");
-      if (xhr.readyState === 4){
-xhr.send(null);
-        if (xhr.status === 200){
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject("Erro na requisição.");
-        }
-      }
-    }
-  })
-}
-
     
 function checaIdade(idade){
-  return new Promisse(function(){
+  return new Promise(function(resolve, reject){
     if (idade >= 18){
       resolve();
     } else {
@@ -37,9 +13,9 @@ function checaIdade(idade){
 }
 
 checaIdade(20)
-  .then(function() {
+  .then(function(resolve) {
     console.log("Maior que 18");
   })
-  .catch(function() {
+  .catch(function(reject) {
     console.log("Menor que 18");
   });
