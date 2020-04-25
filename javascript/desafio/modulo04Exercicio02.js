bafdacd3634c4824d2c88a727bb625c5f45c79d9
Renato Thives <renato.ti@outlook.com.br>
2020-04-25 17:17:57 -0300

    Desafio Módulo 04 - Exercício 02 - Promise.

---
+++ javascript/desafio/modulo04Exercicio02.js
@@ -0,0 +1,31 @@
+/*  Crie uma tela com um <input> que deve receber o nome de um usuário no Github. Após digitar o
+    nome do usuário e clicar no botão buscar a aplicação deve buscar pela API do Github (conforme
+    URL abaixo) os dados de repositórios do usuário e mostrá-los em tela:
+    URL de exemplo: https://api.github.com/users/diego3g/repos
+    Basta alterar "diego3g" pelo nome do usuário.  --*/
+    
+var repositoriesPromisse = function(){
+  return new Promise(function(resolve,reject){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","https://api.github.com/users/renatothives/repos");
+    xhr.send(null);
+
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState === 4){
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject("Erro requisição, status: " + xhr.status);
+        };
+      };
+    };
+  });
+};
+
+repositoriesPromisse()
+  .then(function(response){
+    console.log(response[0].name);
+  })
+  .catch(function(error){
+    console.warn(error);
+  });
