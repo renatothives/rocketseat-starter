16479afdb90badf093fea6cb981cdca7464fec8d
Renato Thives <renato.ti@outlook.com.br>
2020-04-28 20:56:37 -0300

    Classe herança

--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,16 +1,30 @@
-class TodoList {
+class List {
   constructor() {
-    this.todos = [];
+    this.data = [];
   }
 
-  addTodo(){
-    this.todos.push("Novo todo");
-    console.log(this.todos);
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+
+    this.usuario = "Renato";
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
   }
 }
 
 const minhaLista = new TodoList();
 
 document.querySelector("#novotodo").onclick = function() {
-  minhaLista.addTodo();
+  minhaLista.add("Novo item");
 }
+
+minhaLista.mostraUsuario();
