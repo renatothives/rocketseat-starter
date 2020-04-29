4d2deac89a23327a6716bdf35d827be1bea96949
Renato Thives <renato.ti@outlook.com.br>
2020-04-29 14:19:56 -0300

    Operações em Array

--- javascript-es6/bundle.js
+++ javascript-es6/bundle.js
@@ -1,117 +1,23 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var arr = [1, 3, 4, 5, 8, 9, 14]; // map serve para percorrer o arrya/vetor e retornar algo
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr); // consumir todo o vetor e transformar todo ele numa única variável, geralmente um número.
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); // irá verificar se o item corresponde com a validação feita, obrigatoriamente tem que retornar true ou false.
 
-function _createSuper(Derived) { return function () { var Super = _getPrototypeOf(Derived), result; if (_isNativeReflectConstruct()) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter); // irá buscar uma informação no array, quando não encontrato retorna "undefined"
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var List = /*#__PURE__*/function () {
-  function List() {
-    _classCallCheck(this, List);
-
-    this.data = [];
-  }
-
-  _createClass(List, [{
-    key: "add",
-    value: function add(data) {
-      this.data.push(data);
-      console.log(this.data);
-    }
-  }]);
-
-  return List;
-}();
-
-var TodoList = /*#__PURE__*/function (_List) {
-  _inherits(TodoList, _List);
-
-  var _super = _createSuper(TodoList);
-
-  function TodoList() {
-    var _this;
-
-    _classCallCheck(this, TodoList);
-
-    _this = _super.call(this);
-    _this.usuario = "Renato";
-    return _this;
-  }
-
-  _createClass(TodoList, [{
-    key: "mostraUsuario",
-    value: function mostraUsuario() {
-      console.log(this.usuario);
-    }
-  }]);
-
-  return TodoList;
-}(List); // Classe com método estático pode ser acessada diretamente
-
-
-var Matematica = /*#__PURE__*/function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-var minhaLista = new TodoList();
-
-document.querySelector("#novotodo").onclick = function () {
-  minhaLista.add("Novo item");
-};
-
-minhaLista.mostraUsuario();
-console.log(Matematica.soma(3, 5)); // Novas formas de definir variáveis (var, const, let)
-
-var a = 1; // Valor Constante
-// a = 3;  >> isso gera erro, pois não pode ter sua informação reatribuída.
-
-var usuario = {
-  nome: "Diego"
-};
-usuario.nome = "Renato"; // permitido pois isso é mutação do objeto.
-
-console.log(a + " - " + usuario.nome);
-
-function teste(x) {
-  var y = 10; // let é uma "variável" de escopo, só acessível dentro da função.
-
-  console.log(x, y);
-
-  if (true) {
-    var _y = 100; // esse y é valido dentro do if
-
-    console.log(x, _y);
-  }
-
-  console.log(x, y); // volta a aprentar o y da function
-}
-
-teste(5);
+var find = arr.filter(function (item) {
+  return item === 4;
+});
+console.log(find);
--- javascript-es6/main.js
+++ javascript-es6/main.js
@@ -1,65 +1,25 @@
-class List {
-  constructor() {
-    this.data = [];
-  }
+const arr = [1,3,4,5,8,9,14];
 
-  add(data) {
-    this.data.push(data);
-    console.log(this.data);
-  }
-}
+// map serve para percorrer o arrya/vetor e retornar algo
+const newArr = arr.map(function(item,index){
+  return item + index;
+});
+console.log(newArr);
 
-class TodoList extends List {
-  constructor() {
-    super();
+// consumir todo o vetor e transformar todo ele numa única variável, geralmente um número.
+const sum = arr.reduce(function(total,next){
+  return total + next;
+});
+console.log(sum);
 
-    this.usuario = "Renato";
-  }
+// irá verificar se o item corresponde com a validação feita, obrigatoriamente tem que retornar true ou false.
+const filter = arr.filter(function(item){
+  return item % 2 === 0;
+});
+console.log(filter);
 
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-
-// Classe com método estático pode ser acessada diretamente
-class Matematica {
-  static soma(a,b) {
-    return a + b;
-  }
-}
-
-var minhaLista = new TodoList();
-
-document.querySelector("#novotodo").onclick = function() {
-  minhaLista.add("Novo item");
-}
-
-minhaLista.mostraUsuario();
-
-console.log(Matematica.soma(3,5));
-
-
-
-// Novas formas de definir variáveis (var, const, let)
-
-const a = 1; // Valor Constante
-// a = 3;  >> isso gera erro, pois não pode ter sua informação reatribuída.
-
-const usuario = {nome: "Diego"};
-usuario.nome = "Renato"; // permitido pois isso é mutação do objeto.
-
-console.log(a + " - " + usuario.nome);
-
-function teste(x) {
-  let y = 10; // let é uma "variável" de escopo, só acessível dentro da função.
-  console.log(x,y);
-
-  if (true){
-    let y = 100; // esse y é valido dentro do if
-    console.log(x,y);
-  }
-
-  console.log(x,y); // volta a aprentar o y da function
-}
-
-teste(5);
+// irá buscar uma informação no array, quando não encontrato retorna "undefined"
+const find = arr.filter(function(item){
+  return item === 4;
+});
+console.log(find);
