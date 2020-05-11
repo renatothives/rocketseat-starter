13094bfdaaa23c1b814446548cc49b57db7c7e16
Renato Thives <renato.ti@outlook.com.br>
2020-05-11 13:22:33 -0300

    Desafio ES6 Módulo 01 - Exercício 04 - Iniciando.

---
+++ javascript-es6/desafio/modulo01Exercicio04.js
@@ -0,0 +1,19 @@
+// 4.1 Desestruturação simples
+
+// A partir do seguinte objeto:
+const empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+  cidade: 'Rio do Sul',
+  estado: 'SC',
+  }
+};
+
+// Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
+// fim deve ser possível fazer o seguinte:
+
+
+
+console.log(nome); // Rocketseat
+console.log(cidade); // Rio do Sul
+console.log(estado); // SC
