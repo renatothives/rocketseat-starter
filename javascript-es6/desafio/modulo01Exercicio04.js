f5b9a618e5354371586d71337bb1dbadad8cd569
Renato Thives <renato.ti@outlook.com.br>
2020-05-11 13:42:08 -0300

    Desafio ES6 Módulo 01 - Exercício 04 - Finalizado.

--- javascript-es6/desafio/modulo01Exercicio04.js
+++ javascript-es6/desafio/modulo01Exercicio04.js
@@ -12,7 +12,9 @@ const empresa = {
 // Utilize a desestruturação para transformar as propriedades nome, cidade e estado em variáveis, no
 // fim deve ser possível fazer o seguinte:
 
-
+const nome = empresa.nome;
+const cidade = empresa.endereco.cidade;
+const estado = empresa.endereco.estado;
 
 console.log(nome); // Rocketseat
 console.log(cidade); // Rio do Sul
