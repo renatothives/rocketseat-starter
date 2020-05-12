5c3e234b825f936a162b86ae5d77571ffcc3b060
Renato Thives <renato.ti@outlook.com.br>
2020-05-12 14:22:33 -0300

    Desafio ES6 Módulo 01 - Exercício 05 - Finalizado.

--- javascript-es6/desafio/modulo01Exercicio05.js
+++ javascript-es6/desafio/modulo01Exercicio05.js
@@ -6,6 +6,14 @@
 
 const arr = [1, 2, 3, 4, 5, 6];
 
+// REST
+// const { nome, ...resto } = usuario;
+// console.log(nome);
+// console.log(resto);
+// family.filter(person => person.age > 18);
+
+const [x,...y] = arr;
+
 console.log(x); // 1
 console.log(y); // [2, 3, 4, 5, 6]
 
@@ -16,12 +24,18 @@ const usuario = {
   nome: 'Diego',
   idade: 23,
   endereco: {
-  cidade: 'Rio do Sul',
-  uf: 'SC',
-  pais: 'Brasil',
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
   }
 };
 
 // Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+
+const usuario2 = { ...usuario, nome: 'Gabriel'};
+console.log(usuario2);
+
 // Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
 
+const usuario3 = {...usuario, endereco: { ...usuario.endereco, cidade: 'Lontras' } };
+console.log(usuario3);
