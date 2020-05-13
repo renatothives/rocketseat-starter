b7305c697287931a02f5134ced597892ea532612
Renato Thives <renato.ti@outlook.com.br>
2020-05-13 13:45:25 -0300

    Desafio ES6 Módulo 01 - Exercício 07 - Iniciando.

---
+++ javascript-es6/desafio/modulo01Exercicio07.js
@@ -0,0 +1,9 @@
+// Utilize a sintaxe curta de objetos (Object Short Syntax) no seguinte objeto:
+
+const nome = 'Diego';
+const idade = 23;
+const usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul',
+};
